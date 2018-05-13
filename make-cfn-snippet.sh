5512d9656439a24cca6748e865850f669e8cd4e1
lunarxlark <lunarxlark@gmail.com>
2018-05-13 12:52:52 +0000

    Add Resource properties snippets

--- make-cfn-snippet.sh
+++ make-cfn-snippet.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 home=$(cd $(dirname $0);pwd)
-yaml_snip=${home}/yaml.snip
-json_snip=${home}/json.snip
-aws_cfn_doc_repo=$home/aws-cloudformation-user-guide
+aws_cfn_doc_repo=${home}/aws-cloudformation-user-guide
 aws_cfn_doc_dir=${aws_cfn_doc_repo}/doc_source
-
-if [ -e "${yaml_snip}" ];then rm ${yaml_snip}; fi
-if [ -e "${json_snip}" ];then rm ${json_snip}; fi
 
 if [ ! -d "aws-cloudformation-user-guide" ];then 
   git clone https://github.com/awsdocs/aws-cloudformation-user-guide.git
@@ -15,40 +10,52 @@ fi
 
 cd ${aws_cfn_doc_dir}
 
-# make yaml.snip
-for FILE in `grep "^### YAML" aws-resource* | awk -F: '{ print $1 }' | sort -u`
-do
-  echo "snippet " `sed -n 1P $FILE | sed -e "s/^# //g" -e "s/<a .*//g"` >> ${yaml_snip} 
+for file_type in yaml json
+do 
 
-  start=$(expr $(sed -ne '/^### YAML/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==1 { print $1}') + 1)
-  end=$(expr $(sed -ne '/^### YAML/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==2 { print $1}') - 1)
+  # initialize
+  snip=${home}/${file_type}.snip
+  if [ -e "${snip}" ];then rm ${snip}; fi
 
-  sed -ne '/^### YAML/,$p' $FILE | \
-    sed -ne "${start},${end}p" | \
-    sed -e "s/^/  /g" | \
-    sed -e "s/\[//g" | \
-    sed -e "s/\].*)//g" >> ${yaml_snip}
-  echo "" >> ${yaml_snip}
-  echo "" >> ${yaml_snip}
+  # AWS Resource snippets
+  echo "### AWS Resource snippets" >> ${snip}
+  for FILE in `grep "^### ${file_type~~}" aws-resource* | awk -F: '{ print $1 }' | sort -u`
+  do
+    echo "snippet " `sed -n 1P $FILE | sed -e "s/^# //g" -e "s/<a .*//g" -e "s/ /::/g"` >> ${snip} 
+  
+    start=$(expr $(sed -ne '/^### '${file_type~~}'/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==1 { print $1}') + 1)
+    end=$(expr $(sed -ne '/^### '${file_type~~}'/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==2 { print $1}') - 1)
+  
+    sed -ne '/^### '${file_type~~}'/,$p' $FILE | \
+      sed -ne "${start},${end}p" | \
+      sed -e "s/^/  /g" | \
+      sed -e "s/([^)]*)//g" | \
+      sed -e "s/\[//g" -e "s/\]//g" >> ${snip}
+    echo "" >> ${snip}
+    echo "" >> ${snip}
+  done
+ 
+
+  # Resource Properties snippets
+  echo "### Resource Properties snippets" >> ${snip}
+  for FILE in `grep "^### ${file_type~~}" aws-properties-* | awk -F: '{ print $1 }' | sort -u`
+  do
+    echo -n "snippet " >> ${snip}
+    echo -n `sed -n 1P $FILE | sed -e "s/^# //g" -e "s/<a .*//g" -e "s/.* //g"` >> ${snip}
+    echo $FILE | sed -e "s/aws-properties//g" -e "s/.md//g" >> ${snip}
+  
+    start=$(expr $(sed -ne '/^### '${file_type~~}'/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==1 { print $1}') + 1)
+    end=$(expr $(sed -ne '/^### '${file_type~~}'/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==2 { print $1}') - 1)
+  
+    sed -ne '/^### '${file_type~~}'/,$p' $FILE | \
+      sed -ne "${start},${end}p" | \
+      sed -e "s/^/  /g" | \
+      sed -e "s/([^)]*)//g" | \
+      sed -e "s/\[//g" -e "s/\]//g" >> ${snip}
+    echo "" >> ${snip}
+    echo "" >> ${snip}
+  done
 done
 
-
-# make json.snip
-for FILE in `grep "^### JSON" aws-resource* | awk -F: '{ print $1 }' | sort -u`
-do
-  echo "snippet " `sed -n 1P $FILE | sed -e "s/^# //g" -e "s/<a .*//g"` >> ${json_snip} 
-
-  start=$(expr $(sed -ne '/^### JSON/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==1 { print $1}') + 1)
-  end=$(expr $(sed -ne '/^### JSON/,$p' $FILE | grep -n "\`\`\`" | awk -F: 'NR==2 { print $1}') - 1)
-
-  sed -ne '/^### JSON/,$p' $FILE | \
-    sed -ne "${start},${end}p" | \
-    sed -e "s/^/  /g" | \
-    sed -e "s/\[//g" | \
-    sed -e "s/\].*)//g" >> ${json_snip}
-  echo "" >> ${json_snip}
-  echo "" >> ${json_snip}
-done
-
-
+# refact
 sed -i -e "s/ $//g" ${home}/*.snip
