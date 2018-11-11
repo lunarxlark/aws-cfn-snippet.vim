3763ffeee8e530f2ae77c6bd3e4ce6aceb673a16
lunarxlark <lunarxlark@gmail.com>
2018-11-11 13:49:25 +0900

    refact Usage and Env variables

--- make-cfn-snippet.sh
+++ make-cfn-snippet.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-home=$(cd $(dirname $0); pwd)
+# option for below snippet plugins
+# - neosnippet
+# - Ultisnip
+USAGE="
+Unsupported argument. Valid targets are:\n
+  UltiSnip: '-u' or '--ultisnip'\n
+  NeoSnippet: '-n' or '--neosnippet' [default]\n"
 target="${1:--n}"
-aws_cfn_doc_repo="${home}/aws-cloudformation-user-guide"
-aws_cfn_doc_dir="${aws_cfn_doc_repo}/doc_source"
-
 case "${target#-}" in
     n|-neosnippet)
         endsnippet_str=""
@@ -13,24 +16,23 @@ case "${target#-}" in
         endsnippet_str="endsnippet"
         ;;
     *)
-        cat <<- 'EOS'
-			Unsupported argument. Valid targets are:
-			  UltiSnip: '-u' or '--ultisnip'
-			  NeoSnippet: '-n' or '--neosnippet' [default]
-
-		EOS
-
+        echo -e ${USAGE}
         exit 1
         ;;
 esac
 
-git submodule update --init --force
 
-cd "${aws_cfn_doc_dir}"
-# initialize
+# initialize variables
+home=$(cd $(dirname $0); pwd)
+aws_cfn_doc_repo="${home}/aws-cloudformation-user-guide"
+aws_cfn_doc_dir="${aws_cfn_doc_repo}/doc_source"
+
+git submodule update --init --force
 mkdir -p "${home}/snippets/"
 rm -vrf "${home}"/snippets/*
 
+
+# main
 cd "${aws_cfn_doc_dir}"
 for file_type in yaml json
 do
